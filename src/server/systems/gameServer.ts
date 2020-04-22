e066bc15c60f9d7d06e16fbdb3cbb7435f516112
Tobias Wehrum <Tobias.Wehrum@dragonlab.de>
2020-04-23 00:39:26 +0200

    x-forwarded-for, maybe?

--- src/server/systems/gameServer.ts
+++ src/server/systems/gameServer.ts
@@ -30,7 +30,7 @@ export class GameServer {
 
     @bind
     private async onConnection(socket: socketio.Socket) {
-        const address = socket.request.connection.remoteAddress;
+        const address = socket.handshake.headers["x-forwarded-for"] || socket.request.connection.remoteAddress;
         const previousConnection = this.connectionsByAddress.get(address);
         if (previousConnection) {
             socket.disconnect();
